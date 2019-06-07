5a37129db3d007086f42b6383dc1086c820ab88e
Denis Strelets <StreletsDY@gmail.com>
2019-06-07 14:25:00 +0300

    package refactoring

--- app/src/main/java/com/denspark/strelets/cinematrix/not_for_production/HttpController.java
+++ app/src/main/java/com/denspark/strelets/cinematrix/not_for_production/HttpController.java
@@ -1,6 +1,7 @@
 package com.denspark.strelets.cinematrix.not_for_production;
 
 import androidx.lifecycle.MutableLiveData;
+import com.denspark.strelets.cinematrix.api.FilmixApi;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
