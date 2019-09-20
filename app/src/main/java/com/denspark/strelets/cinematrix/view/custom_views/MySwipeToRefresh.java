cfd2f24072fe2e0c14e3b446bc72e5829ea16cd3
Denis Strelets <streletsdy@gmail.com>
2019-09-20 09:38:37 +0300

    Fix for horizontal recycler view in SwipeToRefresh layout issue

---
+++ app/src/main/java/com/denspark/strelets/cinematrix/view/custom_views/MySwipeToRefresh.java
@@ -0,0 +1,40 @@
+package com.denspark.strelets.cinematrix.view.custom_views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.ViewConfiguration;
+
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
+
+public class MySwipeToRefresh  extends SwipeRefreshLayout {
+
+    private int mTouchSlop;
+    private float mPrevX;
+
+    public MySwipeToRefresh(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        mTouchSlop = ViewConfiguration.get(context).getScaledTouchSlop();
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                mPrevX = MotionEvent.obtain(event).getX();
+                break;
+
+            case MotionEvent.ACTION_MOVE:
+                final float eventX = event.getX();
+                float xDiff = Math.abs(eventX - mPrevX);
+
+                if (xDiff > mTouchSlop) {
+                    return false;
+                }
+        }
+
+        return super.onInterceptTouchEvent(event);
+    }
+}
